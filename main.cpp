e99eab42667d2f7e8cf580443a05b880421a827e
hupengxiang <hupengxiang@vivo.com.cn>
2014-09-30 17:43:22 +0800

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	cout<<"I want to learn something about android!\n"<<endl;
+	cout<<"because I want to program a app which is enabled of camera functi	on!\n"<<endl;
+}
